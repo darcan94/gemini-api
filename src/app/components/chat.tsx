d7f9bb8fca4688757c657b72fe17fd828c157b06
darcan94 <darcanf.94@gmail.com>
2024-06-25 16:30:33 -0400

    Ajustes al chat.tsx

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -11,9 +11,7 @@ export default function Chat(){
     const [input, setInput] = useState('');
     const [messages, setMessages] = useState<IMessage[]>([]);
     const [isLoading, setLoading] = useState(false);
-    const chatListRef = useAutoScroll(messages);
-
-    
+    const chatListRef = useAutoScroll(messages);    
 
     const handleKeyDown = (evt: KeyboardEvent<HTMLTextAreaElement>): void => {
         if (evt.key === "Enter" && !evt.shiftKey) {
@@ -42,33 +40,30 @@ export default function Chat(){
             method: 'POST',
             body: JSON.stringify(messages)
         });
-
         
         if(!response.ok){
             throw new Error(
                 await response.text() || "Failed to fetch the chat response."
             );
         }
-       
-         // ------------- With Stream -----------------
-           const reader = response.body?.getReader();
-           const decoder = new TextDecoder("utf-8");
-            let receivedData = await reader?.read();
-            let text = '';
-            while(!receivedData?.done){
-                text += decoder.decode(receivedData?.value);
-                setMessages([
-                    ...messages,
-                    {role: 'model', content: text}
-                ])
-                receivedData = await reader?.read();
-            }  
 
-            setLoading(false);
+        const reader = response.body?.getReader();
+        const decoder = new TextDecoder("utf-8");
+        let receivedData = await reader?.read();
+        let text = '';
+        while(!receivedData?.done){
+            text += decoder.decode(receivedData?.value);
+            setMessages([
+                ...messages,
+                {role: 'model', content: text}
+            ])
+            receivedData = await reader?.read();
+        }  
+        setLoading(false);
     }
 
     return (
-        <div className="w-4/5 h-full mx-auto flex flex-col justify-end gap-5">
+        <div className="w-8/12 h-full mx-auto flex flex-col justify-end gap-5">
             <div ref={chatListRef} className="overflow-y-auto">
                 { messages.map((message, i) => <Message key={i} message={message} />) }
             </div>
